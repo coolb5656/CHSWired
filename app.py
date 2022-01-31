20dc41f6e6ef9c62a9adcb3664254ba741f31d91
coolb5656 <luismata5656@gmail.com>
2022-01-30 22:31:59 -0700

    fix bps

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 from flask_assets import Environment, Bundle
 from flask_login import LoginManager
 
 
-db = SQLAlchemy()
-
 def create_app():
     app = Flask(__name__)
+
+    ####### ASSETS ########
     assets = Environment(app)
 
     bundles = {
@@ -23,13 +22,47 @@ def create_app():
 
     assets.register(bundles)
 
+
+    ############# CONFIG #############
     app.config['SECRET_KEY'] = 'dev'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wbbajeubxwdijf:42d9230539d6322698cb30380c5542d6549909ba71664a7dbb6f064923c0ff79@ec2-3-212-143-188.compute-1.amazonaws.com:5432/ddnpsj9vqfmh7u'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    ######### DB #################
 
+    from models.models import db,student
+    db.init_app(app)
+
+
+    ############ LOGIN ##############
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return student.query.get(int(user_id))
+
+    ###########ADDING ROUTES/BLUEPRINTS#############
     @app.route("/")
     def index():
         return render_template("index.html")
+    
+    # Forms
+    from forms.auth import auth
+    app.register_blueprint(auth, url_prefix="/auth")
+
+    from forms.checkout import checkout
+    app.register_blueprint(checkout, url_prefix="/checkout")
+
+    from forms.reserve import reserve
+    app.register_blueprint(reserve, url_prefix="/reserve")
+
+    # Views
+    from views.admin import admin
+    app.register_blueprint(admin, url_prefix="/admin")
+
+    from views.main import main
+    app.register_blueprint(main)
 
     return app
